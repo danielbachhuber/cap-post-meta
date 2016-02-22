2e5ccadaf26da4c91961e422c129de8c3d8de61b
Daniel Bachhuber <daniel@handbuilt.co>
2016-02-22 12:16:50 -0800

    Initial commit

---
+++ cap-post-meta.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Co-Authors Plus Post Meta
+ * Version: 0.1-alpha
+ * Description: Store Co-Authors Plus data in post meta
+ * Author: Daniel Bachhuber
+ * Author URI: https://handbuilt.co
+ * Plugin URI: https://handbuilt.co
+ * Text Domain: cap-post-meta
+ * Domain Path: /languages
+ * @package Co-Authors Plus Post Meta
+ */
